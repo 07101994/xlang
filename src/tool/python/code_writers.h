00aeecd5acd590498acabc5e42537d9568c310d3
Harry Pierson <hpierson@microsoft.com>
2018-10-09 16:55:59 -0700

    WIP add QI methods for projected pinterfaces

--- src/tool/python/code_writers.h
+++ src/tool/python/code_writers.h
@@ -781,7 +781,18 @@ static int @_%(%* self, PyObject* value, void* /*unused*/)
         w.write("    virtual ~py@() {};\n", type.TypeName());
         w.write("    virtual winrt::Windows::Foundation::IUnknown const& get_unknown() = 0;\n");
         w.write("    virtual std::size_t hash() = 0;\n\n");
-            
+
+        for (auto&& ii : type.InterfaceImpl())
+        {
+            auto name = type.TypeName();
+            if (ii.Interface().type() == TypeDefOrRef::TypeSpec)
+            {
+                auto gt = ii.Interface().TypeSpec().Signature().GenericTypeInst().GenericType();
+                auto td = gt.type() == TypeDefOrRef::TypeDef ? gt.TypeDef() : find_required(gt.TypeRef());
+                w.write("    virtual PyObject* qi_@() = 0;\n", td.TypeName());
+            }
+        }
+
         for (auto&& method : type.MethodList())
         {
             w.write("    virtual PyObject* %(PyObject* args) = 0;\n", method.Name());
@@ -808,6 +819,17 @@ static int @_%(%* self, PyObject* value, void* /*unused*/)
         w.write("py@Impl(%<%> o) : obj(o) {}\n", type.TypeName(), type, bind_list<write_pinterface_type_arg_name>(", ", type.GenericParam()));
         w.write("winrt::Windows::Foundation::IUnknown const& get_unknown() override { return obj; }\n");
         w.write("std::size_t hash() override { return py::get_instance_hash(obj); }\n");
+
+        for (auto&& ii : type.InterfaceImpl())
+        {
+            auto name = type.TypeName();
+            if (ii.Interface().type() == TypeDefOrRef::TypeSpec)
+            {
+                auto gt = ii.Interface().TypeSpec().Signature().GenericTypeInst().GenericType();
+                auto td = gt.type() == TypeDefOrRef::TypeDef ? gt.TypeDef() : find_required(gt.TypeRef());
+                w.write("PyObject* qi_@() override { return py::wrap_pinterface<%>(obj); }\n", td.TypeName(), ii.Interface());
+            }
+        }
 
         write_type_methods<write_pinterface_method_decl>(w, type);
 
